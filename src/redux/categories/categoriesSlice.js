17582bf943617dc52afb31797de09b229eac5af6
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-21 14:38:49 -0500

    Install Redux
    Add redux toolkit lib
    Add store and folders structure for redux
    Add bookSlice
    Add categoriesSlice.js logic reducer for categories

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,10 @@
+// /src/redux/categories/categoriesSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: 'Under construction',
+  reducers: {},
+});
+
+export default categoriesSlice.reducer;
