dfab7f560eaeec934108b37f22bb82c660cabc34
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-21 14:57:41 -0500

    Update categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -3,7 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState: [],
-  reducers: {},
+  reducers: {
+    getStatus: () => 'Under construction',
+  },
 });
 
+export const { getStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
